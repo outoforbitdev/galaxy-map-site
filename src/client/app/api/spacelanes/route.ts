3c84a78a74cee1edb72f7c36a6d835a93b09d01b
Jay Mirecki <43052836+jaymirecki@users.noreply.github.com>
2024-11-15 21:28:31 -0500

    fix(component): center zoom on mouse
    * fix: center zoom on mouse
    
    * chore: lint
    
    * fix: center pointer zoom on initial pointer positions
    
    * chore: lint

--- src/client/app/api/spacelanes/route.ts
+++ src/client/app/api/spacelanes/route.ts
@@ -1,12 +1,8 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  console.log(request);
   const data = await fetch(`${process.env.API_URL}/spacelanes`);
   const spacelanes = await data.json();
-
-  // console.log(data);
-  // console.log(spacelanes);
 
   return new NextResponse(JSON.stringify(spacelanes), {
     status: 200,
